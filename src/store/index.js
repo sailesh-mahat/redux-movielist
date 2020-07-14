1aea1625431201868c76b6bfee94340bb0a98d63
sailesh-mahat <sailesharsenal@gmail.com>
2020-07-14 19:32:15 +0200

    combining reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,21 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
 import movielistReducer from './reducers/movielistReducer';
+import userReducer from './reducers/userReducer';
+import thunk from 'redux-thunk';
 
-const reducer = combineReducers({movies: movielistReducer});
+const middleware = [thunk];
+
+const allReducers = combineReducers({movies: movielistReducer, users: userReducer});
 
 const initialState = {
 
+    users: [],
     movies: {name: "TITANIC"}
 
 };
 
-const store = createStore(reducer, initialState, 
+const store = createStore(allReducers, initialState, compose(applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && 
-    window.__REDUX_DEVTOOLS_EXTENSION__());
+    window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export default store;
