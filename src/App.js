f1d6bf1b20b8d45f119e48eb7c266a49faddccf1
sailesh-mahat <sailesharsenal@gmail.com>
2020-07-14 18:32:44 +0200

    importing actions, connect with state and dispatch, and set up props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
+import {connect} from 'react-redux';
+import updateMovies from './store/actions/updateMovies';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      REDUX MOVIELIST APP
+      <h3>REDUX MOVIELIST APP</h3>
+      <br/>
+      <span
+      style={{color:'green'}}
+      >YOUR CURRENTMOVIE IS: </span>{props.movies.name}
+      <br/>
+      <button onClick={props.updateMovies}>SELECT NEW MOVIE</button>
     </div>
   );
-}
+};
 
-export default App;
+const MapStateToProps = (state) => {
+  return {
+    movies: state.movies
+  };
+
+};
+
+const MapDispatchToProps = (dispatch) => {
+  return {
+    updateMovies: ()=> dispatch(updateMovies)
+  }
+};
+
+export default connect(MapStateToProps, MapDispatchToProps)(App);
--- src/store/index.js
+++ src/store/index.js
@@ -9,8 +9,8 @@ const initialState = {
 
 };
 
-const store = createStore(reducer, initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+const store = createStore(reducer, initialState, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
     window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
