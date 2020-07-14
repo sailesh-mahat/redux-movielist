85e870038fd9c27fd24de83d5fee7f72ed981f6a
sailesh-mahat <sailesharsenal@gmail.com>
2020-07-14 19:38:48 +0200

    importing fetch action

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {connect} from 'react-redux';
 import updateMovies from './store/actions/updateMovies';
+import fetchUsers from './store/actions/fetchUsers'
 
 function App(props) {
   return (
@@ -12,21 +13,25 @@ function App(props) {
       style={{color:'green'}}
       >YOUR CURRENTMOVIE IS: </span>{props.movies.name}
       <br/>
-      <button onClick={props.updateMovies}>SELECT NEW MOVIE</button>
+      <p><button onClick={props.updateMovies}>SELECT NEW MOVIE</button></p>
+      <br/>
+      <button onClick={props.fetchUsers}>FETCH USERS</button>
     </div>
   );
 };
 
 const MapStateToProps = (state) => {
   return {
-    movies: state.movies
+    movies: state.movies,
+    users: state.users
   };
 
 };
 
 const MapDispatchToProps = (dispatch) => {
   return {
-    updateMovies: ()=> dispatch(updateMovies)
+    updateMovies: () => dispatch(updateMovies),
+    fetchUsers: () => dispatch(fetchUsers)
   }
 };
 
