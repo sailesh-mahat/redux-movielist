abfd9de5be5f02bf246a5913c6507015ac6ce9da
sailesh-mahat <sailesharsenal@gmail.com>
2020-07-14 17:56:32 +0200

    store created

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import {createStore, combineReducers} from 'redux';
+import movielistReducer from './reducers/moveilistReducer';
+
+const reducer = combineReducers({movies: movielistReducer});
+
+const initialState = {
+
+    movies: {name: "TITANIC"}
+
+};
+
+const store = createStore(reducer, initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
